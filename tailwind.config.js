47dde2db39f016ab30d52091a997bae004c3e492
Milena Pieńkosz <designbymilencja@gmail.com>
2023-10-04 19:37:24 +0200

    fix(): changed main color

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,14 @@ module.exports = {
       colors: {
         primary: "#050816",
         secondary: "#aaa6c3",
+        third: "#FFBFA5",
         tertiary: "#151030",
         "black-100": "#100d25",
         "black-200": "#090325",
         "white-100": "#f3f3f3",
       },
       boxShadow: {
-        card: "0px 35px 120px -15px #211e35",
+        card: "0 35px 120px -15px #211e35",
       },
       screens: {
         xs: "450px",
