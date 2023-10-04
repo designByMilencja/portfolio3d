5dbd47071c214a7128fecc9eb61694a65aa80061
Milena Pieńkosz <designbymilencja@gmail.com>
2023-10-04 19:36:09 +0200

    feat(Navbar): created navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,57 @@
+import {styles} from "../style.js";
+import {Link} from "react-router-dom";
+import {useState} from "react";
+import {close, logoGold, menu} from "../assets/index.js";
+import {navLinks} from "../constants/index.js";
+
 const Navbar = () => {
+    const [active, setActive] = useState('');
+    const [toggle, setToggle] = useState(false);
+
     return (
-        <div>n</div>
+        <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+            <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+                <Link to="/"
+                      className="flex items-center gap-2"
+                      onClick={() => {
+                          setActive("");
+                          window.scrollTo(0, 0);
+                      }}>
+                    <img src={logoGold}
+                         className="h-[62px] w-[62px]" alt="logo image"/>
+                </Link>
+                <ul className="hidden list-none sm:flex flex-row gap-10">
+                    {navLinks.map((link) => (
+                        <li key={link.id}
+                            className={`${active === link.title ? "text-third" : "text-white"} hover:text-white text-[18px] font-medium cursor-pointer`}
+                            onClick={() => setActive(link.title)}>
+                            <a href={`#${link.id}`}>{link.title}</a>
+                        </li>
+                    ))}
+                </ul>
+                <div className="sm:hidden flex flex-1 justify-end items-center">
+                    <img src={toggle ? close : menu}
+                         alt="icon menu"
+                         className="w-[28px] h-[28px] object-contain cursor-pointer"
+                         onClick={()=>setToggle(!toggle)}/>
+                    <div
+                    className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+                        <ul className="list-none flex justify-end items-start flex-col gap-4">
+                            {navLinks.map((link) => (
+                                <li key={link.id}
+                                    className={`${active === link.title ? "text-third" : "text-white"} font-poppins font-medium cursor-pointer text-[16px]`}
+                                    onClick={() => {
+                                        setToggle(!toggle);
+                                        setActive(link.title);
+                                    }}>
+                                    <a href={`#${link.id}`}>{link.title}</a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
     );
 }
 
