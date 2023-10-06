54973cb627c48a25e439797431acc361fd28c346
Milena Pieńkosz <designbymilencja@gmail.com>
2023-10-06 14:32:29 +0200

    fix(): changed logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import {styles} from "../style.js";
 import {Link} from "react-router-dom";
 import {useState} from "react";
-import {close, logoGold, menu} from "../assets/index.js";
+import { close, l, menu} from "../assets/index.js";
 import {navLinks} from "../constants/index.js";
 
 const Navbar = () => {
@@ -9,7 +9,7 @@ const Navbar = () => {
     const [toggle, setToggle] = useState(false);
 
     return (
-        <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+        <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 secondary-gradient`}>
             <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
                 <Link to="/"
                       className="flex items-center gap-2"
@@ -17,13 +17,13 @@ const Navbar = () => {
                           setActive("");
                           window.scrollTo(0, 0);
                       }}>
-                    <img src={logoGold}
-                         className="h-[62px] w-[62px]" alt="logo image"/>
+                    <img src={l}
+                         className="h-auto w-[82px]" alt="logo image"/>
                 </Link>
                 <ul className="hidden list-none sm:flex flex-row gap-10">
                     {navLinks.map((link) => (
                         <li key={link.id}
-                            className={`${active === link.title ? "text-third" : "text-white"} hover:text-white text-[18px] font-medium cursor-pointer`}
+                            className={`${active === link.title ? "text-white font-bold" : "text-white"} hover:text-tertiary text-[18px] font-medium cursor-pointer`}
                             onClick={() => setActive(link.title)}>
                             <a href={`#${link.id}`}>{link.title}</a>
                         </li>
@@ -35,11 +35,11 @@ const Navbar = () => {
                          className="w-[28px] h-[28px] object-contain cursor-pointer"
                          onClick={()=>setToggle(!toggle)}/>
                     <div
-                    className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+                    className={`${!toggle ? 'hidden' : 'flex'} p-6 tertiary-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
                         <ul className="list-none flex justify-end items-start flex-col gap-4">
                             {navLinks.map((link) => (
                                 <li key={link.id}
-                                    className={`${active === link.title ? "text-third" : "text-white"} font-poppins font-medium cursor-pointer text-[16px]`}
+                                    className={`${active === link.title ? "text-white font-bold" : "text-white"} hover:text-tertiary font-poppins font-medium cursor-pointer text-[16px]`}
                                     onClick={() => {
                                         setToggle(!toggle);
                                         setActive(link.title);
