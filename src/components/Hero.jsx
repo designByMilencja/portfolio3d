b14f6febfd132452713d820eea3f72924d470a41
Milena Pieńkosz <designbymilencja@gmail.com>
2023-10-11 14:45:44 +0200

    feat(Hero): created Hero section with animation for projects

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,24 +1,43 @@
+import { motion } from "framer-motion";
 import {styles} from "../style.js";
-import ComputersCanvas from "./ComputersCanvas.jsx";
+import {hero} from "../constants/index.js";
+import {walk} from "../assets/index.js";
 
 const Hero = () => {
     return (
         <section className="relative w-full h-screen mx-auto">
             <div
-                className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-center gap-5`}>
+                className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-[1200px] mx-auto flex flex-row items-center gap-5`}>
                 <div className="flex flex-col justify-center items-center">
                     <div className="w-3 h-3 bg-secondary"/>
                     <div className="w-1 sm:h-80 h-40 secondary-gradient"/>
                 </div>
-                <div className="text-tertiary flex flex-col font-bold text-xl tracking-wider max-w-[600px]">
-                    <h1 className="text-2xl sm:text-5xl">Hi, I am <span className="mt-5 md:mb-0">Milena Pieńkosz</span>
+                <div className="text-tertiary flex flex-col max-w-[600px] p-4 overflow-hidden">
+                    <h1 className="text-2xl sm:text-3xl font-bold tracking-wider">Hi, I&apos;m <span className="mt-5 md:mb-0">Milena Pieńkosz</span>
                     </h1>
                     <h2 className="text-xl sm:text-2xl my-2 md:my-5">Fullstack Developer</h2>
-                    <p className="text-sm text-secondary sm:text-base p-1">I specialize in frontend development, but I
-                        also have applications in my portfolio. </p>
-                    <p className="text-sm text-secondary sm:text-base p-1">My main passion is combining both of these
-                        aspects using technologies like Next.js.</p>
-                    <ComputersCanvas/>
+                    <div className="my-8">
+                        {hero.map(desc => (
+                            <p key={desc} className="text-sm text-secondary sm:text-base py-3">{desc}</p>
+                        ))}
+                    </div>
+                    <div className="w-full flex justify-center items-center mt-4">
+                        <a href="#projects">
+                            <motion.div
+                                className="flex justify-center items-center"
+                            animate={{
+                                y: [0, 0, 40],
+                            }}
+                            transition={{
+                                duration: 5,
+                                repeat: Infinity,
+                                repeatType: "loop"
+                            }}>
+                                <img className="w-[25px] h-[64px]" src={walk} alt="icon of walking person"/>
+                                <p className="capitalize text-warning tracking-wider">projects?</p>
+                            </motion.div>
+                        </a>
+                    </div>
                 </div>
             </div>
         </section>
