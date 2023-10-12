1be40fba6e4cecf556eddc27e8c6055f3fee8310
Milena Pieńkosz <designbymilencja@gmail.com>
2023-10-12 16:19:05 +0200

    fix(style): changed styles

--- src/style.js
+++ src/style.js
@@ -1,15 +1,15 @@
 const styles = {
     paddingX: "sm:px-16 px-6",
     paddingY: "sm:py-16 py-6",
-    padding: "sm:px-16 px-6 sm:py-16 py-10",
+    padding: "sm:px-10 px-6 sm:py-16 py-10",
 
     heroHeadText:
         "font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
     h2Text:
-        "font-medium text-xl sm:text-2xl text-secondary",
+        "font-medium text-2xl sm:text-3xl md:text-4xl  text-secondary",
 
     sectionText:
-        "text-sm sm:text-base py-3",
+        "text-sm ss:text-base py-3",
     sectionSubText:
         "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider",
 };
