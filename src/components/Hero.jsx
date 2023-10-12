a8f10a97dd4ecf2842a4badaa579a892b6f09c2c
Milena Pieńkosz <designbymilencja@gmail.com>
2023-10-12 16:18:15 +0200

    fix(Hero): changed styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,18 +10,18 @@ const Hero = () => {
                 className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-[1200px] mx-auto flex flex-row items-center gap-5`}>
                 <div className="flex flex-col justify-center items-center">
                     <div className="w-3 h-3 bg-secondary"/>
-                    <div className="w-1 sm:h-80 h-40 secondary-gradient"/>
+                    <div className="w-1 sm:h-60 h-40 secondary-gradient"/>
                 </div>
-                <div className="text-tertiary flex flex-col p-4 overflow-hidden rounded-xl primary-gradient">
-                    <h1 className="text-2xl sm:text-3xl font-bold tracking-wider">Hi, I&apos;m <span className="mt-5 md:mb-0">Milena Pieńkosz</span>
+                <div className="w-full text-tertiary flex flex-col p-6 overflow-hidden rounded-xl primary-gradient">
+                    <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold tracking-wider">Hi, I&apos;m <span className="mt-5 md:mb-0">Milena Pieńkosz</span>
                     </h1>
-                    <h2 className={`${styles.h2Text} my-2 md:my-5`}>Fullstack Developer</h2>
-                    <div className="my-8 max-w-[400px]">
+                    <h2 className={`${styles.h2Text} my-3 md:my-5`}>Fullstack Developer</h2>
+                    <div className="my-4 max-w-[400px]">
                         {hero.map(desc => (
                             <p key={desc} className={`${styles.sectionText}`}>{desc}</p>
                         ))}
                     </div>
-                    <div className="w-full flex justify-center items-center mt-4">
+                    <div className="w-full flex justify-center items-center">
                         <a href="#projects">
                             <motion.div
                                 className="flex justify-center items-center"
