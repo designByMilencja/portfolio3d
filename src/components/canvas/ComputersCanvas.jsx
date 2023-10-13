e0260da4f4295229be296ce8a8be8cde48e46a5b
Milena Pieńkosz <designbymilencja@gmail.com>
2023-10-13 11:54:49 +0200

    fix(ComputersCanvas): changed directory of canvas

--- src/components/canvas/ComputersCanvas.jsx
+++ src/components/canvas/ComputersCanvas.jsx
@@ -2,7 +2,7 @@
 import {Suspense} from "react";
 import {Canvas} from "@react-three/fiber";
 import {OrbitControls, Preload, useGLTF} from "@react-three/drei";
-import CanvasLoader from "./Loader.jsx";
+import CanvasLoader from "../Loader.jsx";
 
 
 
