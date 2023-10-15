f1c5d7bc48ad39a5eb67fc72b1d9a3c5d84ff6b1
Milena Pieńkosz <designbymilencja@gmail.com>
2023-10-15 18:01:57 +0200

    fix(Experience): changed colors

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -8,7 +8,7 @@ import {experiences} from "../constants/index.js";
 
 const ExperienceCard = ({experience}) =>
     <VerticalTimelineElement
-        contentStyle={{background: "#fff", color: "#1C98A1"}}
+        contentStyle={{background: "#1C98A1", color: "#fff"}}
         contentArrowStyle={{borderLeft: "#fff"}}
         date={experience.date}
         iconStyle={{background: experience.iconBg}}
@@ -31,12 +31,12 @@ const ExperienceCard = ({experience}) =>
 
 const Experience = () => {
     return (
-        <section className="w-full rounded-xl secondary-gradient pb-5">
+        <section className="w-full rounded-xl github-gradient pb-5">
             <motion.div
                 variants={textVariant()}
                 className="w-full flex flex-col items-start pt-5">
                 <p className={`${styles.sectionText}`}>What I have done so far</p>
-                <h2 className={`${styles.h2Text} text-tertiary`}>Work Experience</h2>
+                <h2 className={`${styles.h2Text} text-secondary`}>Work Experience</h2>
             </motion.div>
             <div className="mt-20 flex flex-col">
                 <VerticalTimeline>
