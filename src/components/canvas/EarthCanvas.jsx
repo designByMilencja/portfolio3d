5ad525b583af86c1c02a5c8114b0626f9c85141f
Milena Pieńkosz <designbymilencja@gmail.com>
2023-10-16 21:15:39 +0200

    feat(EarthCanvas): added animation of earth

--- src/components/canvas/EarthCanvas.jsx
+++ src/components/canvas/EarthCanvas.jsx
@@ -1,9 +1,18 @@
 import {Canvas} from "@react-three/fiber";
 import {Suspense} from "react";
 import CanvasLoader from "../Loader";
+import {OrbitControls, Preload, useGLTF} from "@react-three/drei";
 
 const Earth = () => {
-
+    const earth = useGLTF('./planet/scene.gltf')
+    return (
+        <primitive
+            object={earth.scene}
+            scale={2.5}
+            position-y={0}
+            position-x={0}
+        />
+    )
 }
 
 const EarthCanvas = () => {
@@ -12,18 +21,25 @@ const EarthCanvas = () => {
             shadows
             frameloop='demand'
             gl={{preserveDrawingBuffer: true}}
-            camera={{}}
+            camera={{
+                fov: 45,
+                near: 0.1,
+                far: 200,
+                position: [-4, 3, 6]
+            }}
         >
-            <Suspense fallback={<CanvasLoader/}>>
+            <Suspense fallback={<CanvasLoader/>}>
                 <OrbitControls
-                autoRotate
-                enableZoom={false}
-                maxPolarAngle={Math.PI / 2}
-                minPolarAngle={Math.PI / 2}
+                    autoRotate
+                    enableZoom={false}
+                    maxPolarAngle={Math.PI / 2}
+                    minPolarAngle={Math.PI / 2}
                 />
-                </Suspense>
-                </Canvas>
-                );
-            }
+                <Earth/>
+                <Preload all />
+            </Suspense>
+        </Canvas>
+    );
+}
 
-                      export default EarthCanvas;
+export default EarthCanvas;
